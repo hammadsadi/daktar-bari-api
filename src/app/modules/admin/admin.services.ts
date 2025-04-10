02dbebbf4e41a80ad86b003bf64d3547d0ce2236
hammadsadi <hammad.sadi@yahoo.com>
2025-04-10 11:55:42 +0600

    Code Refactor and Multiple Search Filed functionality added and also solve field type Error

--- src/app/modules/admin/admin.services.ts
+++ src/app/modules/admin/admin.services.ts
@@ -1,27 +1,34 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 // Init Prisma Client Constructor
 const prisma = new PrismaClient();
 
 const getAllAdminFromDB = async (query: any) => {
+  //  Admin SearchTerm data
+  const andCondition: Prisma.AdminWhereInput[] = [];
+
+  // Admin Searchable Fields
+  const adminSearchAbleFields = ["name", "email"];
+
+  // Check Query Data
+  if (query?.searchTerm) {
+    andCondition.push({
+      OR: adminSearchAbleFields?.map((field) => ({
+        [field]: {
+          contains: query?.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  // Make Object Data  Using ANT Operator
+  const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
+
+  // Get Admin Info
   const result = await prisma.admin.findMany({
     // Search Admin By Name or Email
-    where: {
-      OR: [
-        {
-          name: {
-            contains: query?.searchTerm,
-            mode: "insensitive",
-          },
-        },
-        {
-          email: {
-            contains: query?.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
-    },
+    where: whereCondition,
   });
   return result;
 };
