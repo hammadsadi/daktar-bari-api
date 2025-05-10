9552aedcb59bcfc76eb44cfa67543b22d03c8c86
hammadsadi <hammad.sadi@yahoo.com>
2025-05-10 10:57:16 +0600

    Updated Schedule Create Role

--- src/app/modules/schedule/schedule.routes.ts
+++ src/app/modules/schedule/schedule.routes.ts
@@ -2,12 +2,18 @@ import { NextFunction, Request, Response, Router } from "express";
 
 import validatedRequest from "../../shared/validatedRequest";
 import { ScheduleControllers } from "./schedule.controllers";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 // Init Route
 const scheduleRouter = Router();
 
 // Create Schedule
-scheduleRouter.post("/create", ScheduleControllers.createSchedule);
+scheduleRouter.post(
+  "/create",
+  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  ScheduleControllers.createSchedule
+);
 
 // Export Routes
 export const ScheduleRoutes = scheduleRouter;
